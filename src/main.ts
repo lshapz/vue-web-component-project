2d4aecb40570a7aba9afbd0fb41feb86dbc3b813
Laura Shapiro <laurashapiro01@gmail.com>
2019-03-20 15:44:58 -0400

    so the counter works, the pie chart doesn't render right as custom, probably d3 weirdness

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 import PieChart from './components/PieChart.vue';
+import VueCounter from './components/VueCounter.vue';
 import wrap from '@vue/web-component-wrapper';
 
 Vue.config.productionTip = false
 
-// const CustomElement = wrap(Vue, PieChart);
-// window.customElements.define('my-custom-element', CustomElement);
+
+
+const CustomElementPie= wrap(Vue, PieChart);
+window.customElements.define('my-custom-pie-chart', CustomElementPie);
+
+
+const CustomElementCount= wrap(Vue, VueCounter);
+window.customElements.define('my-custom-counter', CustomElementCount);
 
 
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+// new Vue({
+//   render: h => h(App)
+// }).$mount('#app')
